769500e664ec3be1696833a2a28dc0d7c224ff8c
Chuong <chuong@wee.vn>
2025-05-19 11:31:22 +0700

    add change

--- __init__.py
+++ __init__.py
@@ -3,11 +3,29 @@ from .nodes.auto.downloader import AutoModelDownloader
 from .nodes.cai.cai_download import CivitAIDownloader
 import os
 
+class GetImageSize:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            }
+        }
+
+    RETURN_TYPES = ("INT", "INT")
+    RETURN_NAMES = ("width", "height")
+    FUNCTION = "execute"
+    CATEGORY = "essentials"
+
+    def execute(self, image):
+        return (image.shape[2], image.shape[1],)
+
 # Node mappings
 NODE_CLASS_MAPPINGS = { 
     "HF Downloader": HFDownloader,
     "Auto Model Downloader": AutoModelDownloader,
     "CivitAI Downloader": CivitAIDownloader,
+    "GetImageSize+": GetImageSize,
 }
 
 # Display names
@@ -15,6 +33,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "HF Downloader": "HF Download",
     "Auto Model Downloader": "Auto Model Finder (Experimental)",
     "CivitAI Downloader": "CivitAI Download",
+    "GetImageSize+": "Get Image Size",
 }
 
 # Web directory for JavaScript files
